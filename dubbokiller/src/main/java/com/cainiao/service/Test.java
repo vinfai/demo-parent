a2bd73e6e967189e4a17be6bc78280e204a559df
方文辉 <wenhui.fang@gewara.com>
2016-05-19 14:25:57 +0800

    Initial commit

---
+++ dubbokiller/src/main/java/com/cainiao/service/Test.java
@@ -0,0 +1,12 @@
+package com.cainiao.service;
+
+import com.alibaba.dubbo.registry.dubbo.DubboRegistry;
+
+/**
+ * @author vinfai
+ * @since 2016/5/19
+ */
+public class Test {
+    public static void main(String[] args) {
+    }
+}
